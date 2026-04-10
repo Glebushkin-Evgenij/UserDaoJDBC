96df0f0653759d8afdb96169603b77a4ecdc8794
Evgenij Glebushkin <ewgenij.glebushkin@yandex.ru>
2026-04-10 21:16:43 +0300

    One commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,107 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+
+    private static final UserDaoJDBCImpl instance = new UserDaoJDBCImpl();
+    private static final String DELETE_USER_SQL = "DELETE FROM users WHERE id = ?";
+    private static final String SELECT_USER_SQL = "SELECT * FROM users";
+    private static final String SAVE_USER_SQL = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
+    private static final String CLEAN_TABLE_SQL = "DELETE FROM users";
+    private static final String DROP_TABLE_SQL = "DROP TABLE IF EXISTS users";
+    private static final String CREATE_TABLE_SQL = """
+            CREATE TABLE IF NOT EXISTS users (
+            id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+            name varchar(255),
+            lastName varchar(255),
+            age int
+            )""";
+
+
     public UserDaoJDBCImpl() {
 
     }
 
-    public void createUsersTable() {
+    Connection connection = Util.getConnection();
 
+    public static UserDaoJDBCImpl getInstance() {
+        return instance;
+    }
+
+    public void createUsersTable() {
+        try(var preparedStatement = connection.prepareStatement(CREATE_TABLE_SQL)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void dropUsersTable() {
-
+        try(var preparedStatement = connection.prepareStatement(DROP_TABLE_SQL)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        try (var preparedStatement = connection.prepareStatement(SAVE_USER_SQL)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (var preparedStatement = connection.prepareStatement(DELETE_USER_SQL)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+
+        try (var preparedStatement = connection.prepareStatement(SELECT_USER_SQL)) {
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            User user = new User();
+            while (resultSet.next()) {
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                users.add(user);
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        try(var preparedStatement = connection.prepareStatement(CLEAN_TABLE_SQL)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -28,6 +28,16 @@ public class User {
         this.age = age;
     }
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "age=" + age +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
     public Long getId() {
         return id;
     }
